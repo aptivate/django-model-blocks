e8a8051e786b8edde70ac18ccea09deab7fde5bf
Daniel Levy <daniell@aptivate.org>
2012-03-05 14:54:40 +0000

    Added support for fields with choices.

--- model_blocks/templatetags/model_nodes.py
+++ model_blocks/templatetags/model_nodes.py
@@ -135,6 +135,9 @@ class ModelDetailNode (BaseModelBlockNode):
             
             label = field.verbose_name
             value = getattr(instance, field.name)
+            if field.choices:
+                method_name = "get_%s_display" % field.name
+                value = getattr(instance, method_name)()
             is_list = False
             is_link = (type(field).__name__ in ('URLField', 'FileField'))
             model = instance._meta.module_name
